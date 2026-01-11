4a8328a220a55f0a150dd16f3da347c09b1fb74c
Arnaud Saint-Léger <arnaudsaintleger@gmail.com>
2026-01-11 21:29:21 +0100

    404

---
+++ mapping.js
@@ -0,0 +1,27 @@
+// mapping.js
+const pathMapping = {
+    // Exemple pour Marguerite Delemer (1876)
+        "individus/delemer-marguerite-1876/recits/1917-journal-premiere-guerre-mondiale-marguerite-delemer.html":
+        "individus/DELEMER Marguerite (1876)/Récits/1917 Journal de la première guerre mondiale de Marguerite Delemer.html",
+
+    "individus/delemer-marguerite-1876/archives/permis-de-conduire-1913.html":
+        "individus/DELEMER Marguerite (1876)/Archives/Permis de conduire 1913.html",
+
+    "individus/delemer-marguerite-1876/archives/otage-1918.html":
+        "individus/DELEMER Marguerite (1876)/Archives/Otage en 1918.html",
+
+    // Exemple pour Jean Dupont (1902)
+    "individus/dupont-jean-1902/actes/naissance-jean-dupont.html":
+        "individus/DUPONT Jean (1902)/Actes/Naissance - Jean Dupont.html",
+
+    "individus/dupont-jean-1902/recits/mobilisation-1943.html":
+        "individus/DUPONT Jean (1902)/Récits/Fascicule de mobilisation 1943.html",
+
+    // Exemple pour Marie Martin (1895)
+    "individus/martin-marie-1895/actes/mariage-marie-martin.html":
+        "individus/MARTIN Marie (1895)/Actes/Mariage - Marie Martin.html",
+
+    "individus/martin-marie-1895/photos/portrait-marie-martin-1920.html":
+        "individus/MARTIN Marie (1895)/Photos/Portrait de Marie Martin (1920).html"
+};
+
